e6b7c50feb5b7c32da3805de01858653ae9f71e5
2i-7 <niina.umekita@gmail.com>
2025-03-23 22:51:46 +0900

    新しく作ったメモは上に積まれるようにした

--- src/components/MemoList.tsx
+++ src/components/MemoList.tsx
@@ -17,7 +17,7 @@ const MemoList: React.FC<MemoListProps> = ({ memos, onDelete, onSelect, onEdit }
   return (
     <div className="memo-list">
       <h2>Memo List</h2>
-      {memos.map((memo) => (
+      {memos.slice().reverse().map((memo) => (
         <div key={memo.id} className="memo-item" onClick={() => onSelect(memo)}>
           <h3>{memo.title}</h3>
           <p>{memo.body}</p>
